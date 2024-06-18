e52e2d71954cbcf78e24ad89f079a669a4626303
anemptyemptiness <103761193+anemptyemptiness@users.noreply.github.com>
2024-06-18 15:46:26 +0300

    fixed checking revenue

--- src/autoposting/check_for_revenue.py
+++ src/autoposting/check_for_revenue.py
@@ -12,43 +12,48 @@ def creating_new_loop_for_checking_revenue(global_loop, bot: Bot):
 
 async def send_revenue_report_by_N_days(bot: Bot):
     while True:
-        date_now = datetime.now(tz=timezone(timedelta(hours=3.0))).date()
-        how_many_days_ago = (
-                date_now - await AsyncOrm._check_data_from_finances()
-        ).days
+        try:
+            date_now = datetime.now(tz=timezone(timedelta(hours=3.0))).date()
+            how_many_days_ago_on_places = [
+                (place_id, (date_now - last_update_date).days) for place_id, last_update_date in await AsyncOrm._check_data_from_finances()
+            ]
 
-        if how_many_days_ago >= settings.DAYS_FOR_FINANCES_CHECK:
-            await AsyncOrm.set_data_to_finances()
+            for place_id, days_difference in how_many_days_ago_on_places:
+                if days_difference >= settings.DAYS_FOR_FINANCES_CHECK:
+                    await AsyncOrm.set_data_to_finances_by_place(place_id=place_id)
 
-            data = await AsyncOrm.get_data_from_finances()
+                    data = await AsyncOrm.get_data_from_finances_by_place(place_id=place_id)
 
-            for title, _, last_money, updated_money, updated_at in data:
-                chat_id = settings.REVENUE_CHAT_ID
-                difference = updated_money - last_money
-                last_money = f"{int(last_money):,}".replace(",", " ")
-                updated_money = f"{int(updated_money):,}".replace(",", " ")
+                    for title, _, last_money, updated_money, updated_at in data:
+                        chat_id = settings.REVENUE_CHAT_ID  # chat-id группы, куда бот будет присылать отчеты по выручке
+                        difference = updated_money - last_money
+                        last_money = f"{int(last_money):,}".replace(",", " ")
+                        updated_money = f"{int(updated_money):,}".replace(",", " ")
 
-                report: str = "📊Статистика по росту выручки\n"
-                report += f"<b>от</b> {updated_at.strftime('%d.%m.%y')} <b>до</b> {date_now.strftime('%d.%m.%y')}\n\n"
+                        report: str = "📊Статистика по росту выручки\n"
+                        report += f"<b>от</b> {updated_at.strftime('%d.%m.%y')} <b>до</b> {date_now.strftime('%d.%m.%y')}\n\n"
 
-                report += f"🏚Точка: <b>{title}</b>\n└"
-                report += f"Выручка {updated_at.strftime('%d.%m.%y')}: <em><b>{last_money}₽</b></em>\n└"
-                report += f"Выручка {date_now.strftime('%d.%m.%y')}: <em><b>{updated_money}₽</b></em>\n\n"
+                        report += f"🏚Точка: <b>{title}</b>\n└"
+                        report += f"Выручка {updated_at.strftime('%d.%m.%y')}: <em><b>{last_money}₽</b></em>\n└"
+                        report += f"Выручка {date_now.strftime('%d.%m.%y')}: <em><b>{updated_money}₽</b></em>\n\n"
 
-                is_normal = True if difference > 0 else False
+                        is_normal = True if difference > 0 else False
 
-                difference = f"{int(difference):,}".replace(",", " ")
-                report += f"Разница составила: <em><b>{difference}₽</b></em> "
+                        difference = f"{int(difference):,}".replace(",", " ")
+                        report += f"Разница составила: <em><b>{difference}₽</b></em> "
 
-                report += f"{'🟢' if is_normal else '🔴'}\n\n"
-                report += f"Результат: <em>{'все в норме✅' if is_normal else 'нужно смотреть камеры⚠️'}</em>"
+                        report += f"{'🟢' if is_normal else '🔴'}\n\n"
+                        report += f"Результат: <em>{'все в норме✅' if is_normal else 'нужно смотреть камеры⚠️'}</em>"
 
-                await bot.send_message(
-                    chat_id=chat_id,
-                    text=report,
-                    parse_mode="html",
-                )
+                        await bot.send_message(
+                            chat_id=chat_id,
+                            text=report,
+                            parse_mode="html",
+                        )
 
+                    await asyncio.sleep(60 * 60 * 24)  # спим 1 день
+                else:
+                    await asyncio.sleep(60 * 60 * 24)  # спим 1 день
             await asyncio.sleep(60 * 60 * 24)  # спим 1 день
-        else:
+        except Exception:
             await asyncio.sleep(60 * 60 * 24)  # спим 1 день
--- src/db/queries/dao/dao.py
+++ src/db/queries/dao/dao.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, and_, func, delete
+from sqlalchemy import select, update, and_, func, delete
 from sqlalchemy import Numeric
 from sqlalchemy.orm import joinedload
 from sqlalchemy.dialects.postgresql import insert
@@ -275,7 +275,7 @@ class AsyncOrm:
             return res.all()
 
     @staticmethod
-    async def set_data_to_finances():
+    async def set_data_to_finances_default():
         async with async_session() as session:
             time_now = datetime.now(tz=timezone(timedelta(hours=3.0))).date()
             time_N_days_ago = time_now - timedelta(days=settings.DAYS_FOR_FINANCES_CHECK) + timedelta(days=1)
@@ -296,96 +296,108 @@ class AsyncOrm:
             data_from_reports = {int(place_id): float(summary) for place_id, summary in res.all()}
 
             # На этом этапе я получил новые выручки
-            # за прошлые 2 недели
+            # за прошлые N дней
             await session.flush()
 
             # Если не пришло никакого результата из Reports,
-            # то просто заполняю Finances дефолтными значениями
+            # то Reports пустая, а это значит, что ничего делать не будем
+            # и выйдем из функции
             if not data_from_reports:
-                # Если Reports еще пустая, то ничего не делаем
-                if not await AsyncOrm._check_reports_for_null():
-                    await session.commit()
-                    return
-
-                place_ids_query = (
-                    select(Reports.place_id)
-                )
-                res = await session.execute(place_ids_query)
-                place_ids_dict = {"place_id": int(place_id) for place_id in res.scalars().all()}
-
-                await session.flush()
-
-                stmt = (
-                    insert(Finances)
-                    .values(place_ids_dict)
-                )
-                await session.execute(stmt)
-                await session.commit()
                 return
 
             # Если таблица Finances была пустая, то я вставлю в
             # last_money и updated_money одинаковые значения,
             # так как еще не было суммы новее, поэтому они должны быть равны
-            if not await AsyncOrm._check_finances_for_null():
+            stmt = (
+                insert(Finances)
+                .values(
+                    [
+                        {"place_id": place_id, "last_money": updated_money, "updated_money": updated_money}
+                        for place_id, updated_money in data_from_reports.items()
+                    ]
+                )
+            )
+
+            await session.execute(stmt)
+            await session.commit()
+
+            # Выхожу из метода, так как нет смысла делать что-либо дальше
+            # Я обновил всё, что можно на данный момент
+            await session.commit()
+            return
+
+    @staticmethod
+    async def set_data_to_finances_by_place(place_id: int):
+        async with async_session() as session:
+            time_now = datetime.now(tz=timezone(timedelta(hours=3.0))).date()
+            time_N_days_ago = time_now - timedelta(days=settings.DAYS_FOR_FINANCES_CHECK) + timedelta(days=1)
+
+            sum_of_revenue_query = (
+                select(
+                    Reports.place_id,
+                    func.sum(Reports.revenue)
+                )
+                .select_from(Reports)
+                .filter(
+                    and_(Reports.report_date.between(time_N_days_ago, time_now), Reports.place_id == place_id)
+                )
+                .group_by(Reports.place_id)
+            )
+
+            res = await session.execute(sum_of_revenue_query)
+            data_from_reports = {int(place_id): float(summary) for place_id, summary in res.all()}
+
+            # если такой точки еще не было в Finances,
+            # то ее нужно там создать, а далее выйти из функции
+            if not await AsyncOrm._check_finances_for_null_by_place(place_id=place_id):
                 stmt = (
                     insert(Finances)
                     .values(
                         [
-                            {"place_id": place_id, "last_money": updated_money, "updated_money": updated_money}
+                            {"place_id": place_id, "updated_money": updated_money, "last_money": 0}
                             for place_id, updated_money in data_from_reports.items()
                         ]
                     )
                 )
-
                 await session.execute(stmt)
                 await session.commit()
-
-                # Выхожу из метода, так как нет смысла делать что-либо дальше
-                # Я обновил всё, что можно на данный момент
                 return
 
-            # Если таблица Finances не пустая, то нужно в
-            # старое значение last_money поставить
+            last_updated_money_query = (
+                select(
+                    Finances.updated_money
+                )
+                .select_from(Finances)
+                .filter_by(place_id=place_id)
+            )
+            res = await session.execute(last_updated_money_query)
+            data_from_finances_for_place = {
+                "place_id": place_id, "last_money": float(res.scalar()),
+            }
+
+            # На данном этапе я получил старое
+            # значение updated_money для конкретной точки, чтобы
+            # вставить их в last_money для этой же точки
+            await session.flush()
+
+            stmt = (
+                insert(Finances)
+                .values(data_from_finances_for_place)
+                .on_conflict_do_update(
+                    index_elements=[Finances.place_id],
+                    set_={
+                        "last_money": insert(Finances).excluded.last_money,
+                    }
+                )
+            )
+            await session.execute(stmt)
+
+            # На этом этапе last_money содержит
             # старое значение updated_money
-            else:
-                last_updated_money_query = (
-                    select(
-                        Finances.place_id,
-                        Finances.updated_money
-                    )
-                    .select_from(Finances)
-                )
-                res = await session.execute(last_updated_money_query)
-                data_from_finances = {int(place_id): float(last_updated_money) for place_id, last_updated_money in res.all()}
-
-                # На данном этапе я получил старые
-                # значения updated_money, чтобы
-                # вставить их в last_money
-                await session.flush()
-
-                stmt = (
-                    insert(Finances)
-                    .values(
-                        [
-                            {"place_id": place_id, "last_money": last_updated_money}
-                            for place_id, last_updated_money in data_from_finances.items()
-                        ]
-                    )
-                    .on_conflict_do_update(
-                        index_elements=[Finances.place_id],
-                        set_={
-                            "last_money": insert(Finances).excluded.last_money,
-                        }
-                    )
-                )
-                await session.execute(stmt)
-
-            # На этом этапе last_money содержат
-            # старые значения updated_money
             #
             # Теперь мне нужно в updated_money
-            # вставить новые значения, которые пришли
-            # из таблицы Reports (новые выручки точек за 2 прошлые недели)
+            # вставить новое значение, которые пришло
+            # из таблицы Reports (новые выручки точки за прошлые N дней)
             await session.flush()
 
             stmt = (
@@ -404,19 +416,22 @@ class AsyncOrm:
                 )
             )
 
-            # Я обновил updated_money новыми значениями,
+            # Я обновил updated_money новым значением,
             # и теперь можно спокойно выходить из функции
             await session.execute(stmt)
             await session.commit()
 
     @staticmethod
-    async def get_data_from_finances():
+    async def get_data_from_finances_by_place(place_id: int):
         async with async_session() as session:
+            time_now = datetime.now(tz=timezone(timedelta(hours=3.0))).date()
+
             query = (
                 select(
                     Finances,
                 )
                 .select_from(Finances)
+                .filter_by(place_id=place_id)
                 .options(joinedload(Finances.place))
             )
 
@@ -431,37 +446,61 @@ class AsyncOrm:
                 ) for data in res.scalars().all()
             ]
 
+            # обновлю дату updated_at в Finances, так как я получил
+            # и вывел всю старую нужную информацию в этой функции
+
+            stmt = (
+                update(Finances).values(updated_at=time_now).filter_by(place_id=place_id)
+            )
+            await session.execute(stmt)
+            await session.commit()
+
             return result
 
     @staticmethod
     async def _check_data_from_finances():
         async with async_session() as session:
+            # если таблица Finances пустая, то заполняю ее дефолтными значениями
             if not await AsyncOrm._check_finances_for_null():
-                await AsyncOrm.set_data_to_finances()
+                await AsyncOrm.set_data_to_finances_default()
 
             query = (
                 select(
-                    Finances.updated_at
+                    Places.id,
+                    Finances.updated_at,
                 )
                 .select_from(Finances)
-                .limit(1)
+                .join(Places, Finances.place_id == Places.id)
             )
             res = await session.execute(query)
-            result = res.scalars().one()
+            result = res.all()
 
             await session.commit()
             return result
 
     @staticmethod
-    async def _check_finances_for_null():
+    async def _check_finances_for_null_by_place(place_id: int):
         async with async_session() as session:
             query = (
                 select(Finances)
+                .select_from(Finances)
+                .filter_by(place_id=place_id)
             )
             res = await session.execute(query)
             await session.commit()
 
             return res.scalars().all()
+
+    @staticmethod
+    async def _check_finances_for_null():
+        async with async_session() as session:
+            query = (
+                select("*")
+                .select_from(Finances)
+            )
+            res = await session.execute(query)
+
+            return res.all()
 
     @staticmethod
     async def _check_reports_for_null():
